13af6ec602a2b738145cf517c879f8365ecbb5f6
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-04-06 22:05:24 -0300

    Separando atividade 1 e 2 para nota da ac1

---
+++ Atividade 2/src/main/java/com/example/ac1atividade2/entities/Funcionario.java
@@ -0,0 +1,69 @@
+package com.example.ac1atividade2.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Funcionario implements Serializable{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private int id;
+    private String nome;
+    private String cargo;
+    private String setor;
+    private Double salario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario [cargo=" + cargo + ", id=" + id + ", nome=" + nome + ", salario=" + salario + ", setor="
+                + setor + "]";
+    }
+    
+}
