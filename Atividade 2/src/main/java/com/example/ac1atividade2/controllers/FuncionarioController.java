3b5e7200f1fad6ed1b4dd8c41b3fffdb40bf21c4
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-04-06 22:43:27 -0300

    2 - Correção do Post de /funcionarios e finalização da view

--- Atividade 2/src/main/java/com/example/ac1atividade2/controllers/FuncionarioController.java
+++ Atividade 2/src/main/java/com/example/ac1atividade2/controllers/FuncionarioController.java
@@ -9,9 +9,9 @@ import com.example.ac1atividade2.services.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
@@ -33,7 +33,7 @@ public class FuncionarioController {
     }
 
     @PostMapping("/funcionarios")
-    public ModelAndView postFuncionarios(@RequestBody Funcionario funcionario) {
+    public ModelAndView postFuncionarios(@ModelAttribute Funcionario funcionario) {
         
         ModelAndView mv = new ModelAndView("funcionarios");
         List<Funcionario> funcionarios = service.addFuncionario(funcionario);
